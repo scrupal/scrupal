8246dfbeb381536699f1c08ee2b1d0dfc565fe7a
Reid Spencer <reid.spencer@j9tech.com>
2013-09-10 17:47:50 -0400

    Initial commit - boilerplate. * Output of "play new scrupal" from play command * Output of "gen-idea" from sbt-idea plugin * Some initial builder code.

---
+++ project/Build.scala
@@ -0,0 +1,122 @@
+import dispatch./
+import sbt._
+import sbt.Keys._
+import play.Project._
+import sbt.Opts.resolver
+
+/**
+ * Settings for building Scrupal. These are common settings for each sub-project.
+ * Only put things in here that must be identical for each sub-project. Otherwise,
+ * Specialize below in the definition of each Project object.
+ */
+object BuildSettings
+{
+  val appName = "scrupal"
+  val buildVersion = "0.1"
+
+  val buildSettings = Defaults.defaultSettings ++ Seq (
+    // credentials += Credentials(Path.userHome / ".ivy2" / ".credentials"),
+    javacOptions ++= Seq(
+      "-J-Xmx1024m",
+	    "-Xlint"
+    ),
+    organization    := "scrupal.org",
+    // publishTo := Some(Resolvers.MyArtifactHost),
+    scalacOptions   ++= Seq(
+      "-J-Xss32m",
+      "-feature",
+      "-language:implicitConversions",
+      "-language:postfixOps",
+      "-language:reflectiveCalls",
+      "-unchecked",
+      "-deprecation",
+      "-encoding", "utf8",
+      "-Ywarn-adapted-args"
+    ),
+    scalaVersion    := "2.10",
+    shellPrompt     := ShellPrompt.buildShellPrompt,
+    version         := buildVersion
+  )
+}
+
+/**
+ * Augment the Play shell prompt with the Shell prompt which show the current project,
+ * git branch and build version
+ */
+object ShellPrompt
+{
+  object devnull extends ProcessLogger {
+    def info (s: => String) {}
+    def error (s: => String) { }
+    def buffer[T] (f: => T): T = f
+  }
+
+  val current = """\*\s+([^\s]+)""".r
+
+  def gitBranches = ("git branch --no-color" lines_! devnull mkString)
+
+  val buildShellPrompt = {
+    (state: State) => {
+      val currBranch = current findFirstMatchIn gitBranches map (_ group(1)) getOrElse "-"
+      val currProject = Project.extract (state).currentProject.id
+      "%s:%s:%s> ".format (currBranch, currProject, BuildSettings.buildVersion)
+    }
+  }
+}
+
+
+object Resolvers
+{
+  val sbt_plugin_releases     = Resolver.url("SBT Plugin Releases", url("http://repo.scala-sbt.org/scalasbt/sbt-plugin-releases"))(Resolver.ivyStylePatterns)
+  val pk11_scratch            = "Google PK11-Scratch" at "http://pk11-scratch.googlecode.com/svn/trunk"
+  val typesafe_releases       = "Typesafe Releases" at "http://repo.typesafe.com/typesafe/releases/"
+  val sonatype_releases       = "Sonatype Releases"  at "http://oss.sonatype.org/content/repositories/releases"
+  val scala_lang              = "Scala Language" at "http://mvnrepository.com/artifact/org.scala-lang/"
+
+//val geolocation             = "geolocation repository" at "http://blabluble.github.com/modules/releases/",
+
+  val all_resolvers           = Seq ( sbt_plugin_releases, pk11_scratch, typesafe_releases, sonatype_releases,
+	                                    scala_lang )
+}
+
+object Dependencies
+{
+  val play_plugins_redis      = "com.typesafe"        %% "play-plugins-redis"     % "2.1.1"
+  val play2_reactivemongo     = "org.reactivemongo"   %% "play2-reactivemongo"    % "0.9"
+  val mailer_plugin           = "com.typesafe"        %% "play-plugins-mailer"    % "2.1.0"
+
+//val icu4j                   = "com.ibm.icu"          % "icu4j"                  % "51.1"
+//val geolocation             =  "com.edulify"        %% "geolocation"            % "1.1.0"
+
+  // Test Libraries
+  val specs2                  = "org.specs2"          %% "specs2"                 % "2.1.1"       % "test"
+
+}
+
+object ScrupalBuild extends Build {
+
+  import BuildSettings._
+  import Resolvers._
+  import Dependencies._
+
+  val printClasspath = TaskKey[File]("print-class-path")
+
+  def print_class_path = (target, fullClasspath in Compile, compile in Compile) map { (out, cp, analysis) =>
+    println(cp.files.map(_.getCanonicalPath).mkString("\n"))
+    println("----")
+    println(analysis.relations.allBinaryDeps.toSeq.mkString("\n"))
+    println("----")
+    println(out)
+    out
+  }
+
+  lazy val scrupal = play.Project(
+    appName,
+    path = file("."),
+    settings = ( buildSettings ++ Seq (
+      resolvers := all_resolvers,
+      libraryDependencies := Seq ( specs2, play2_reactivemongo, play_plugins_redis ),
+      printClasspath <<= print_class_path
+    ))
+  )
+}
---
+++ project/plugins.sbt
@@ -0,0 +1,12 @@
+// Comment to get more information during initialization
+logLevel := Level.Info
+
+// The Typesafe repository
+resolvers += "Typesafe repository" at "http://repo.typesafe.com/typesafe/releases/"
+
+resolvers += "Sonatype respository" at "https://oss.sonatype.org/content/repositories/releases/"
+
+// Use the Play sbt plugin for Play projects
+addSbtPlugin("play" % "sbt-plugin" % "2.1.3")
+
+addSbtPlugin("com.github.mpeltonen" %% "sbt-idea" % "1.5.1")
