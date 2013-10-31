ec419ddfe922166e83e9e087230589c0f68ea83e
Reid Spencer <reid@viritude.com>
2013-10-31 18:23:35 -0400

    Bump the "fast" complexity numbers on Hashers again to minimize false negatives. #14

--- app/scrupal/utils/Hasher.scala
+++ app/scrupal/utils/Hasher.scala
@@ -135,7 +135,7 @@ object PBKDF2Hasher extends Hasher {
   // Provide the name of this hasher
   override val id = 'PBKDF2
 
-  def defaultIterations = if ( Hash.fastMode ) 5000 else 25000
+  def defaultIterations = if ( Hash.fastMode ) 8000 else 25000
 
 
   override def hash(plainText: String, saltine: Option[String], complexity: Option[Long]) = {
@@ -158,7 +158,7 @@ object BCryptHasher extends Hasher {
   // Provide the name of this hasher
   override val id = 'BCrypt
 
-  def defaultRounds = if ( Hash.fastMode ) 1 else 10
+  def defaultRounds = if ( Hash.fastMode ) 4 else 10
 
   // Set up some parameterization of the bcrypt algorithm
   override def hash(plainText: String, saltine: Option[String], complexity: Option[Long]) = {
