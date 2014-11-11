e23491197a5cf4c1b41fa6326f4892b839217a32
Reid Spencer <reid@viritude.com>
2014-11-11 16:04:05 -0500

    Simpler Walking Code - InProgress #22 * Prepare PathWalker for hopefully allowing editing of BSONDocuments.

--- scrupal-utils/src/scrupal/utils/PathWalker.scala
+++ scrupal-utils/src/scrupal/utils/PathWalker.scala
@@ -9,68 +9,73 @@ trait PathWalker[D,A,V] extends ScrupalComponent {
   protected def indexArray(index: Int, a: A) : Option[V]
   protected def arrayLength(a: A) : Int
 
-  def lookup(path: String, document: D) : Option[V] = {
+  protected def error(msg: String, head: Seq[String], paths: Seq[String]) = {
+    toss(s"${msg} at path '${head.mkString(".")}.${
+      paths.head
+    }' with remaining path elements: ${
+      paths.tail.mkString(".")
+    }")
+  }
 
-    def error(msg: String, head: Seq[String], paths: Seq[String]) = {
-      toss(s"${msg} at path '${head.mkString(".")}.${
-        paths.head
-      }' with remaining path elements: ${
-        paths.tail.mkString(".")
-      }")
+  protected def getAsDocument(head: Seq[String], paths: Seq[String], value: V) : D = {
+    if (isDocument(value)) {
+      asDocument(value)
+    } else {
+      error("Selected value was not a document so it could not be indexed", head, paths)
     }
+  }
 
-    def getAsDocument(head: Seq[String], paths: Seq[String], value: V) : D = {
-      if (isDocument(value)) {
-        asDocument(value)
-      } else {
-        error("Selected value was not a document so it could not be indexed", head, paths)
-      }
+  protected def getAsArray(head: Seq[String], paths: Seq[String], value: V) : A = {
+    if (isArray(value)) {
+      asArray(value)
+    } else {
+      error("Selected value was not an array so it could not be indexed", head, paths)
     }
+  }
 
-    def resolve(head: Seq[String], paths: Seq[String], doc: D) : Option[V] = {
-      if (paths.isEmpty)
-        return None
-      val name = paths.head
-      require(paths.length > 0)
-      val deref : Option[V] = if (name.endsWith("]")) {
+  protected def walk(head: Seq[String], paths: Seq[String], parents: Seq[D], doc: D) : (String, Int, Seq[D], D, Option[V]) = {
+    if (paths.isEmpty)
+      return ("", -1, parents, doc, None)
+    val name = paths.head
+    val result = {
+      if (paths.head.endsWith("]")) {
         val beginIndex = name.lastIndexOf("[") + 1
         if (beginIndex <= 0)
           error("Malformed array subscripts", head, paths)
         else {
           val endIndex = name.length - 1
           val index = name.substring(beginIndex, endIndex).toInt
-          val rootName = name.substring(0, beginIndex-1)
+          val rootName = name.substring(0, beginIndex - 1)
           indexDoc(rootName, doc) match {
             case Some(value) ⇒
-              if (isArray(value)) {
-                val anArray = asArray(value)
-                if (index < 0 || index > arrayLength(anArray)-1)
-                  error(s"Array subscript out of bounds ", head, paths)
-                else {
-                  indexArray(index, anArray)
-                }
-              } else
-                error("Selected value was not an array so it could not be indexed", head, paths)
-            case None ⇒ None
+              val anArray = getAsArray(head, paths, value)
+              if (index < 0 || index > arrayLength(anArray) - 1)
+                error(s"Array subscript out of bounds ", head, paths)
+              else {
+                val indexed = indexArray(index, anArray)
+                (rootName, index, indexed)
+              }
+            case None ⇒ (rootName, index, None)
           }
         }
       } else {
-        indexDoc(name, doc) match {
-          case Some(value) ⇒ Some(value)
-          case None ⇒ None
-        }
-      }
-      if (paths.length == 1) {
-        deref
-      } else if (deref.isEmpty) {
-        None
-      } else {
-        val nextDoc = getAsDocument(head, paths, deref.get)
-        resolve(head :+ name, paths.tail, nextDoc)
+        val indexed = indexDoc(name, doc)
+        (name, -1, indexed)
       }
     }
+    if (paths.length == 1 || !result._3.isDefined)
+      (result._1, result._2, parents, doc, result._3)
+    else {
+      val nextDoc = getAsDocument(head, paths, result._3.get)
+      val nextParents : Seq[D] = parents :+ doc
+      walk(head :+ name, paths.tail, nextParents, nextDoc)
+    }
+  }
+
+  def lookup(path: String, document: D) : Option[V] = {
     val parts = path.split("\\.").toSeq
-    resolve(Seq.empty[String], parts, document)
+    val (name, index, parents, doc, value) = walk(Seq.empty[String], parts, Seq.empty[D], document)
+    value
   }
 
 }
