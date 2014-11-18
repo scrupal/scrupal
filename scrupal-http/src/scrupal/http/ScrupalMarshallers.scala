e9c0697ccc972992a728e262343776940cda02a2
Reid Spencer <reid@viritude.com>
2014-11-18 15:07:32 -0500

    Get Stream Marshalling Working - InProgress #21 * Clearly, delegated marshallers need a content type. * Also, one must actually invoke a marshaller to get data marshalled! * Make StreamingResponseActor a top level class for easier Actor instantiation

--- scrupal-http/src/scrupal/http/ScrupalMarshallers.scala
+++ scrupal-http/src/scrupal/http/ScrupalMarshallers.scala
@@ -33,144 +33,147 @@ import scala.util.control.NonFatal
 
 trait ScrupalMarshallers extends BasicMarshallers with MetaMarshallers {
 
-  def makeMarshallable(fr: Future[Result[_]])(implicit scrupal: Scrupal) : ToResponseMarshallable = {
+  def makeMarshallable(fr: Future[Result[_]])(implicit scrupal: Scrupal): ToResponseMarshallable = {
     scrupal.withActorExec { (as, ec, to) ⇒
-      val marshaller1 : ToResponseMarshaller[Result[_]] = mystery_marshaller(as, ec, to)
-      val marshaller2 = futureMarshaller[Result[_]](marshaller1,ec)
+      val marshaller1: ToResponseMarshaller[Result[_]] = mystery_marshaller(as, ec, to)
+      val marshaller2 = futureMarshaller[Result[_]](marshaller1, ec)
       ToResponseMarshallable.isMarshallable(fr)(marshaller2)
     }
   }
 
-  def makeMarshallable(r: Result[_])(implicit scrupal: Scrupal) : ToResponseMarshallable = {
+  def makeMarshallable(r: Result[_])(implicit scrupal: Scrupal): ToResponseMarshallable = {
     scrupal.withActorExec { (as, ec, to) ⇒
-      val marshaller1 : ToResponseMarshaller[Result[_]] = mystery_marshaller(as, ec, to)
+      val marshaller1: ToResponseMarshaller[Result[_]] = mystery_marshaller(as, ec, to)
       ToResponseMarshallable.isMarshallable(r)(marshaller1)
     }
   }
 
-  val html_ct = ContentType(MediaTypes.`text/html`,HttpCharsets.`UTF-8`)
-  val text_ct = ContentType(MediaTypes.`text/plain`,HttpCharsets.`UTF-8`)
+  val html_ct = ContentType(MediaTypes.`text/html`, HttpCharsets.`UTF-8`)
+  val text_ct = ContentType(MediaTypes.`text/plain`, HttpCharsets.`UTF-8`)
 
-  implicit val html_marshaller : ToResponseMarshaller[HtmlResult] = {
-    ToResponseMarshaller.delegate[HtmlResult,String](html_ct) { h ⇒ h.payload.body }
+  implicit val html_marshaller: ToResponseMarshaller[HtmlResult] = {
+    ToResponseMarshaller.delegate[HtmlResult, String](html_ct) { h ⇒ h.payload.body}
   }
 
-  implicit val text_marshaller : ToResponseMarshaller[TextResult] =
-    ToResponseMarshaller.delegate[TextResult,String](text_ct) { h ⇒ h.payload }
+  implicit val text_marshaller: ToResponseMarshaller[TextResult] =
+    ToResponseMarshaller.delegate[TextResult, String](text_ct) { h ⇒ h.payload}
 
-  implicit val error_marshaller : ToResponseMarshaller[ErrorResult] =
-    ToResponseMarshaller.delegate[ErrorResult,String](text_ct) { e ⇒
+  implicit val error_marshaller: ToResponseMarshaller[ErrorResult] =
+    ToResponseMarshaller.delegate[ErrorResult, String](text_ct) { e ⇒
       s"Error: ${e.disposition.id.name}: ${e.payload}"
     }
 
-  implicit val exception_marshaller : ToResponseMarshaller[ExceptionResult] =
-    ToResponseMarshaller.delegate[ExceptionResult,String](text_ct) { e ⇒
+  implicit val exception_marshaller: ToResponseMarshaller[ExceptionResult] =
+    ToResponseMarshaller.delegate[ExceptionResult, String](text_ct) { e ⇒
       s"Exception: ${e.payload}"
     }
 
-  implicit def stream_marshaller(
-    implicit arf: ActorRefFactory, ec: ExecutionContext, timeout:  Timeout) : ToResponseMarshaller[StreamResult] =
-    ToResponseMarshaller.delegate[StreamResult,EnumeratorResult]() { s: StreamResult ⇒
+  implicit def stream_marshaller(ct: ContentType)(
+    implicit arf: ActorRefFactory, ec: ExecutionContext, timeout: Timeout): ToResponseMarshaller[StreamResult] =
+    ToResponseMarshaller.delegate[StreamResult, EnumeratorResult](ct) { s: StreamResult ⇒
       val e = Enumerator.fromStream(s.payload)
       EnumeratorResult(e, s.mediaType, s.disposition)
-    } (enumerator_marshaller(arf,ec,timeout))
+    }(enumerator_marshaller(arf, ec, timeout))
 
   implicit def mystery_marshaller(
-    implicit arf: ActorRefFactory, ec: ExecutionContext, timeout:  Timeout) : ToResponseMarshaller[Result[_]] = {
+    implicit arf: ActorRefFactory, ec: ExecutionContext, timeout: Timeout): ToResponseMarshaller[Result[_]] = {
     ToResponseMarshaller[Result[_]] {
       (value, trmc) => {
         value match {
-          case h: HtmlResult ⇒ html_marshaller
-          case t: TextResult ⇒ text_marshaller
-          case x: ExceptionResult ⇒ exception_marshaller
-          case s: StreamResult ⇒ stream_marshaller
-          case e: EnumeratorResult ⇒ enumerator_marshaller
+          case h: HtmlResult ⇒ html_marshaller(h, trmc)
+          case t: TextResult ⇒ text_marshaller(t, trmc)
+          case x: ExceptionResult ⇒ exception_marshaller(x, trmc)
+          case s: StreamResult ⇒
+            val m = stream_marshaller(s.mediaType)
+            m(s, trmc)
+          case e: EnumeratorResult ⇒
+            val m = enumerator_marshaller
+            m(e, trmc)
         }
       }
     }
   }
 
-  implicit def futureMarshaller[T](implicit m: ToResponseMarshaller[T], ec: ExecutionContext) :
+  implicit def futureMarshaller[T](implicit m: ToResponseMarshaller[T], ec: ExecutionContext):
   ToResponseMarshaller[Future[T]] =
     ToResponseMarshaller[Future[T]] { (value, ctx) ⇒
       value.onComplete {
-        case Success(v)     ⇒ m(v, ctx)
+        case Success(v) ⇒ m(v, ctx)
         case Failure(error) ⇒ ctx.handleError(error)
       }
     }
 
-  implicit def enumerator_marshaller(
-    implicit arf: ActorRefFactory, ec: ExecutionContext, timeout:  Timeout) : ToResponseMarshaller[EnumeratorResult] =
-      ToResponseMarshaller[EnumeratorResult] { (value, trmc) => {
-
-      val responseStreamerActor: ActorRef = arf.actorOf(Props(classOf[EnumeratorMarshallingActor], value, trmc))
-
-      def byteArrayIteratee(responseStreamerActor: ActorRef): Iteratee[Array[Byte], Unit] = {
+  implicit def enumerator_marshaller(implicit arf: ActorRefFactory, ec: ExecutionContext, timeout: Timeout)
+  : ToResponseMarshaller[EnumeratorResult] = {
+    ToResponseMarshaller[EnumeratorResult] { (value, trmc) =>
+      def consumePayload(streamActor: ActorRef): Iteratee[Array[Byte], Unit] = {
         def continuation(input: Input[Array[Byte]]): Iteratee[Array[Byte], Unit] = input match {
           case Input.Empty => Cont[Array[Byte], Unit](continuation)
-          case Input.El(array) => Iteratee.flatten((responseStreamerActor ? array) map (_ => Cont[Array[Byte], Unit](continuation)))
-          case Input.EOF => responseStreamerActor ! Input.EOF; Done((), Input.EOF)
+          case Input.El(array) => Iteratee.flatten(
+            (streamActor ? array) map (_ => Cont[Array[Byte], Unit](continuation)))
+          case Input.EOF => streamActor ! Input.EOF; Done((), Input.EOF)
         }
-
         Cont[Array[Byte], Unit](continuation)
       }
 
-      value.payload |>>> byteArrayIteratee(responseStreamerActor) onFailure {
-        case NonFatal(error) => responseStreamerActor ! error
+      val streamingResponseActor: ActorRef = arf.actorOf(Props(classOf[StreamingResponseActor], value, trmc))
+
+      value.payload |>>> consumePayload(streamingResponseActor) onFailure {
+        case NonFatal(error) => streamingResponseActor ! error
       }
     }
   }
+}
 
-  class EnumeratorMarshallingActor(value: EnumeratorResult, trmc: ToResponseMarshallingContext) extends Actor with
-                                                                                                    ActorLogging  {
-    object ChunkSent
+class StreamingResponseActor(value: EnumeratorResult, trmc: ToResponseMarshallingContext) extends Actor with
+                                                                                                  ActorLogging  {
+  object ChunkSent
 
-    def receive = {
-      case data: Array[Byte] =>
-        context.become(
-          waitingForResponder(
-            trmc.startChunkedMessage(
-              HttpResponse(entity = HttpEntity(value.mediaType, data)),
-              ack = Some(ChunkSent)
-            ),
-            sender()
-          )
+  def receive = {
+    case data: Array[Byte] =>
+      context.become(
+        waitingForResponder(
+          trmc.startChunkedMessage(
+            HttpResponse(entity = HttpEntity(value.mediaType, data)),
+            ack = Some(ChunkSent)
+          ),
+          sender()
         )
-      case Input.EOF => {
-        trmc.marshalTo(HttpResponse(entity = HttpEntity(value.mediaType, HttpData.Empty)))
-        context.stop(self)
-      }
-      case error: Throwable => {
-        trmc.handleError(error)
-        context.stop(self)
-      }
+      )
+    case Input.EOF => {
+      trmc.marshalTo(HttpResponse(entity = HttpEntity(value.mediaType, HttpData.Empty)))
+      context.stop(self)
+    }
+    case error: Throwable => {
+      trmc.handleError(error)
+      context.stop(self)
+    }
+  }
+
+  def waitingForData(responder: ActorRef): Actor.Receive = {
+    case data: Array[Byte] => {
+      responder ! MessageChunk(data).withAck(ChunkSent)
+      context.become(waitingForResponder(responder, sender))
+    }
+    case Input.EOF => {
+      responder ! ChunkedMessageEnd
+      context.stop(self)
+    }
+    case error: Throwable => {
+      trmc.handleError(error)
+      context.stop(self)
+    }
+  }
+
+  def waitingForResponder(responder: ActorRef, requestor: ActorRef): Actor.Receive = {
+    case ChunkSent => {
+      requestor ! ChunkSent
+      context.become(waitingForData(responder))
     }
 
-    def waitingForData(responder: ActorRef): Actor.Receive = {
-      case data: Array[Byte] => {
-        responder ! MessageChunk(data).withAck(ChunkSent)
-        context.become(waitingForResponder(responder, sender))
-      }
-      case Input.EOF => {
-        responder ! ChunkedMessageEnd
-        context.stop(self)
-      }
-      case error: Throwable => {
-        trmc.handleError(error)
-        context.stop(self)
-      }
-    }
-
-    def waitingForResponder(responder: ActorRef, requestor: ActorRef): Actor.Receive = {
-      case ChunkSent => {
-        requestor ! ChunkSent
-        context.become(waitingForData(responder))
-      }
-
-      case event: Http.ConnectionClosed => {
-        log.warning("Binary response streaming stopped due to {}", event)
-        context.stop(self)
-      }
+    case event: Http.ConnectionClosed => {
+      log.warning(s"Response streaming stopped because: $event")
+      context.stop(self)
     }
   }
 }
