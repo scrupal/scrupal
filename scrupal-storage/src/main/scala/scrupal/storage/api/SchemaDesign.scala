678a2db95a299babd99e6b7f9f829909385fb6b7
Reid Spencer <reid@reactific.com>
2015-06-11 13:05:18 -0400

    Make SchemaDesign compile again.

--- scrupal-storage/src/main/scala/scrupal/storage/api/SchemaDesign.scala
+++ scrupal-storage/src/main/scala/scrupal/storage/api/SchemaDesign.scala
@@ -1,13 +1,13 @@
 /** ********************************************************************************************************************
   * This file is part of Scrupal, a Scalable Reactive Web Application Framework for Content Management                 *
-  *                                                                                                                  *
+  *                                                                                                                    *
   * Copyright (c) 2015, Reactific Software LLC. All Rights Reserved.                                                   *
-  *                                                                                                                  *
+  *                                                                                                                    *
   * Licensed under the Apache License, Version 2.0 (the "License"); you may not use this file except in compliance     *
   * with the License. You may obtain a copy of the License at                                                          *
-  *                                                                                                                  *
-  *   http://www.apache.org/licenses/LICENSE-2.0                                                                     *
-  *                                                                                                                  *
+  *                                                                                                                    *
+  *  http://www.apache.org/licenses/LICENSE-2.0                                                                        *
+  *                                                                                                                    *
   * Unless required by applicable law or agreed to in writing, software distributed under the License is distributed   *
   * on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the License for  *
   * the specific language governing permissions and limitations under the License.                                     *
@@ -18,7 +18,6 @@ package scrupal.storage.api
 
 import scrupal.utils.Validation._
 
-import scala.concurrent.{ Future, ExecutionContext }
 
 /** A Schema Design
   *
@@ -75,11 +74,11 @@ abstract class SchemaDesign extends Validator[Schema] {
       for (name ← requiredNames) yield {
         collections.get(name) match {
           case None ⇒
-            Error(TypedLocation(schema).select(name), schema, s"Collection $name does not exist")
+            StringFailure(TypedLocation(schema).select(name), schema, s"Collection $name does not exist")
           case Some(c) ⇒ {
             validateCollection(schema, name, c) match {
               case None    ⇒ null
-              case Some(s) ⇒ Error(TypedLocation(schema).select(name), schema, s)
+              case Some(s) ⇒ StringFailure(TypedLocation(schema).select(name), schema, s)
             }
           }
         }
